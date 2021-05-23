a152cd6784e2653c71830814eb786f66f108f9e5
Irene <irenesis123@gmail.com>
2021-05-23 00:27:22 -0400

    [it] meta url changed

--- components/layout/layout-component.jsx
+++ components/layout/layout-component.jsx
@@ -1,16 +1,16 @@
-import Head from "next/head";
+import Head from 'next/head';
 
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router';
 
 // components
-import NavBar from "../navigation/nav-bar-component";
-import PersistFooter from "../footer/footer-component";
-import ConditionalCTA from "../conditionalCTA/conditional-cta.component";
+import NavBar from '../navigation/nav-bar-component';
+import PersistFooter from '../footer/footer-component';
+import ConditionalCTA from '../conditionalCTA/conditional-cta.component';
 
-const name = "Irene Truong";
+const name = 'Irene Truong';
 const description =
-  "Irene Truong is a Web Developer specializing in Front-End servicing the Toronto and Greater Toronto Area.";
-export const siteTitle = "Irene Truong | Web Developer";
+  'Irene Truong is a Web Developer specializing in Front-End servicing the Toronto and Greater Toronto Area.';
+export const siteTitle = 'Irene Truong | Web Developer';
 
 export default function Layout({ children }) {
   const router = useRouter();
@@ -27,7 +27,7 @@ export default function Layout({ children }) {
         <meta name="og:title" content={siteTitle} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content="/images/OG_Image.jpg" />
-        <meta property="og:url" content="https://irenetruong.com/"></meta>
+        <meta property="og:url" content="https://irenetruong.tech/"></meta>
         <meta name="twitter:card" content="summary_large_image" />
         <link rel="icon" href="images/favicon.ico" />
       </Head>
